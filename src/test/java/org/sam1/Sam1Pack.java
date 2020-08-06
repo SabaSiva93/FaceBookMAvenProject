4c336343d16d71948c5c51d3aad331ada65e3f7e
sabasiva93 <sabasv38@gmail.com>
2020-08-06 14:15:01 +0530

    task completed

---
+++ src/test/java/org/sam1/Sam1Pack.java
@@ -0,0 +1,13 @@
+package org.sam1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sam1Pack {
+
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saba Siva\\eclipse-workspace\\FacebookMaven\\driver\\chromedriver.exe");
+		 WebDriver driver =new ChromeDriver();
+	}
+}
