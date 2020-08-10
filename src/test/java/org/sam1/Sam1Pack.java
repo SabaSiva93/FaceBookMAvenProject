4fed682cf630ed4c335ff3583641fde6eb18dfc9
sabasiva93 <sabasv38@gmail.com>
2020-08-10 20:01:48 +0530

    login verified and cheked

--- src/test/java/org/sam1/Sam1Pack.java
+++ src/test/java/org/sam1/Sam1Pack.java
@@ -10,6 +10,7 @@ public class Sam1Pack {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saba Siva\\eclipse-workspace\\FacebookMaven\\driver\\chromedriver.exe");
 		 WebDriver driver =new ChromeDriver();
 		 driver.get("https://www.facebook.com/");
+		 System.out.println("hiii....");
 		 
 	}
 }
